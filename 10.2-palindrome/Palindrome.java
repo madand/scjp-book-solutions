a165df9cfa846987512bfff4cf148d866044376c
madand <dev@madand.net>
2021-08-07 01:25:10 +0300

    Add solution for 10.2

---
+++ 10.2-palindrome/Palindrome.java
@@ -0,0 +1,37 @@
+
+public class Palindrome {
+    public static void main(String[] args) {
+        if (args.length != 1) {
+            System.out.println("Please provide exactly one argument - a word.");
+            System.exit(1);
+        }
+
+        String inputWord = args[0];
+
+        System.out.println("The word \"" + inputWord + "\" " +
+                (isPalindrome(inputWord) ? "IS" : "IS NOT") + " a palindrome.");
+    }
+
+    /**
+     * Check whether the given word is a palindrome. A word is a palindrome if it reads the same backwards.
+     * Example: madam.
+     *
+     * @param word the input word.
+     * @return true if the word is a palindrome, false - otherwise.
+     */
+    static boolean isPalindrome(String word) {
+        String reversed = new StringBuilder(word).reverse().toString();
+
+        return word.equals(reversed);
+    }
+
+    /**
+     * Check whether the given number is even.
+     *
+     * @param n the input number.
+     * @return true if the number is even, false if odd.
+     */
+    private static boolean isOdd(int n) {
+        return n % 2 != 0;
+    }
+}
---
+++ 10.2-palindrome/README.md
@@ -0,0 +1,23 @@
+Solution for exercise 10.2 (Palindrome)
+================================
+
+Compile & run
+-------------
+
+``` shell
+> javac Palindrome.java
+> java Palindrome <word>
+```
+
+Substitute `<word>` with the word you want to test for being a palindrome.
+
+Sample output
+-------------
+
+``` shell
+> java Palindrome something
+The word "something" IS NOT a palindrome.
+
+> java Palindrome madam
+The word "madam" IS a palindrome.
+```
